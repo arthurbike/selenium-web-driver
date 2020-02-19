ed2715173ce3ce962cdf2bb2ff9dc30d274a3973
arthurbike <55563210+arthurbike@users.noreply.github.com>
2020-02-19 14:10:44 -0300

    Update SeleniumTest.java

--- src/main/java/webdriver/SeleniumTest.java
+++ src/main/java/webdriver/SeleniumTest.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class SeleniumTest {
 
 	public static void main(String[] args) {
-		//                                            (          caminho do driver do goolgle chrome  'chromedriver.exe'                        )
+		//                                                   (   caminho do driver do goolgle chrome  'chromedriver.exe'    )
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\arthu\\git\\selenium-web-driver\\src\\main\\java\\webdriver\\chromedriver.exe");
 
 		try {
